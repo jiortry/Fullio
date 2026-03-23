dd4f74877e240fa223a13fb6bcc2550bbacc5fa2
Jerry Panunzio <jerryworksonline@gmail.com>
2026-03-23 16:42:45 +0100

    Initial Commit

---
+++ Fullio/FullioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullioApp.swift
+//  Fullio
+//
+//  Created by QWERTY on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FullioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
